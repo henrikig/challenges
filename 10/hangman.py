9348ee970d5a5126a2b154bf24fe5594e79f4dfd
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2020-02-16 09:32:16 +1300

    Started class based Hangman game

--- 10/hangman.py
+++ 10/hangman.py
@@ -10,10 +10,36 @@ ALLOWED_GUESSES = len(HANG_GRAPHICS)
 PLACEHOLDER = '_'
 
 
-class Hangman(object):
-    pass 
+class Hangman:
+    def __init__(self, word=None):
+        if word is None:
+            word = get_word()
+        self.word = list(word.lower())
+        self.allowed_guesses = ALLOWED_GUESSES
+        self.guessed = []
+        self.correct_letters = []
+        self.wrong_letters = []
+        self.won = False
 
-# or use functions ...
+    def letter_input(self, letter):
+        letter = letter.lower()
+        if letter not in ASCII:
+            prompt_input("This is not a valid letter")
+        elif letter in self.guessed:
+            prompt_input("This letter is already guessed")
+        else:
+            self.guessed.append(letter)
+            if letter in self.word:
+                self.correct_letters.append(letter)
+            else:
+                self.wrong_letters.append(letter)
+
+    def play(self):
+        if len(self.wrong_letters) <= self.allowed_guesses and not self.won:
+
+
+    def prompt_input(self, message):
+        print(message)
 
 
 if __name__ == '__main__':
@@ -22,5 +48,6 @@ if __name__ == '__main__':
     else:
         word = get_word()
     print(word)
+    print(ASCII)
 
     # init / call program
