89c6ff7dcf77f9faa2c8b3b421c6d97dc4cccd57
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2020-03-11 14:35:11 +1300

    Very basic Flask application for todos

--- 15/app.py
+++ 15/app.py
@@ -1,26 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
-# persistence
-#
-# could go with serialization
-# import shelve
-# or:
-# import pickle
-#
-# could go with stdlib DB
-# import sqlite3
-# 
-# could go with ORM
-# $ pip install flask_sqlalchemy
-# add:
-# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    pass
+    todo = None
+    if request.method == 'POST':
+        todo = request.form.get('todo')
+    with open('todo.txt', 'r') as f:
+        todos = f.read()
+        todos = json.loads(todos) if todos else dict()
+    if todo:
+        if todo in todos:
+            todos[todo] = not todos[todo]
+        else:
+            todos[todo] = False
+        with open('todo.txt', 'w') as f:
+            json.dump(todos, f)
+
+    return render_template('todos.html', todos=todos)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', name=username.capitalize())
+
 
 
 if __name__ == '__main__':
